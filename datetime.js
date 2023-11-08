212a73a594665ee9db6c261566d03e7fbeef6ddc
Ivan Zubkov <43518470+thundiverter@users.noreply.github.com>
2023-11-09 01:35:21 +0300

    Add files via upload

--- datetime.js
+++ datetime.js
@@ -1,18 +1,7 @@
 const datetimeElem = document.querySelector('#date-time');
 const date = new Date();
-const months = {
-    0: 'января',
-    1: 'февраля',
-    2: 'марта',
-    3: 'апреля',
-    4: 'мая',
-    5: 'июня',
-    6: 'июля',
-    7: 'августа',
-    8: 'сентября',
-    9: 'октября',
-    10: 'ноября',
-    11: 'декабря'
-}
+const months = [
+    'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+];
 
 datetimeElem.innerText = `Учебный предмет • ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
